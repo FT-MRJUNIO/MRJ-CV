b956d5fe7419dade4323eaace96d825fd0cbee10
MarlonJunio-NSJBI <marlon.junio@nsjbi.com.ph>
2026-09-08 10:43:50 +0800

    Upload files from New folder

--- js/cv-exporter.js
+++ js/cv-exporter.js
@@ -1,5 +1,26 @@
 ﻿window.cvExporter = {
     generatePDF: function () {
-        window.print();
+        const element = document.querySelector('.cv-paper');
+
+        const opt = {
+            margin: 0,
+            filename: 'Resume.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: {
+                scale: 2,
+                useCORS: true,
+                letterRendering: true, 
+                logging: false
+            },
+            jsPDF: {
+                unit: 'mm',
+                format: 'a4',
+                orientation: 'portrait'
+            },
+            pagebreak: { mode: 'avoid-all', before: '#pageel' }
+        };
+
+        html2pdf().set(opt).from(element).toPdf().get('pdf').then(function (pdf) {
+        }).save();
     }
 };
